5ea6f8e84328313fb56427fadf159bddc8c81ebf
vinca <vinca.r@yandex.com>
2020-05-25 22:32:27 +0900

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,38 @@ import './style.css';
 import kao from 'kaomojilib';
 
 const onClick = e => {
-  if (navigator.clipboard) {
-    navigator.clipboard.writeText(e.currentTarget.value);
+  copy(e.currentTarget.value);
+}
+
+const copy = (str) => {
+  if (!str || typeof (str) != "string") {
+    return "";
   }
+
+  //strを含んだtextareaをbodyタグの末尾に設置
+  const textarea = document.createElement('textarea');
+  textarea.id = 'tmp_copy';
+  textarea.style = 'position:fixed;right:100vw;font-size:16px;';
+  textarea.readOnly = true;
+  textarea.value = str;
+  document.body.appendChild(textarea);
+
+  //select()でtextarea内の文字を選択
+  textarea.select();
+
+  //rangeでtextarea内の文字を選択
+  var range = document.createRange();
+  range.selectNodeContents(textarea);
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+  textarea.setSelectionRange(0, 999999);
+
+  //execCommandを実施
+  document.execCommand("copy");
+
+  //textareaを削除
+  textarea.parentNode.removeChild(textarea);
 }
 
 const col = document.querySelector('.container .row .column');
@@ -14,7 +43,7 @@ Object.entries(kao.library).forEach(([text, content]) => {
   const button = document.createElement('button');
   button.textContent = content.icon;
   button.value = content.icon;
-  button.classList.add('button','button-outline');
+  button.classList.add('button', 'button-outline');
   button.addEventListener('click', onClick, false);
   col.appendChild(button);
 })
